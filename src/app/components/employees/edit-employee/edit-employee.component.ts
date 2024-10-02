7272051be8d5a6328ca2a90078e5cd4d945a8d0b
Ruwanya Hettiarachchi <ruwanyahettiarachchi@gmail.com>
2024-10-02 12:50:29 +0530

    add a delete emloyee component and complete the edit employee component

--- src/app/components/employees/edit-employee/edit-employee.component.ts
+++ src/app/components/employees/edit-employee/edit-employee.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from 'src/app/models/employee.model';
+import { EmployeesService } from 'src/app/Services/employees.service';
 
 @Component({
   selector: 'app-edit-employee',
   templateUrl: './edit-employee.component.html',
   styleUrls: ['./edit-employee.component.css']
 })
-export class EditEmployeeComponent {
+export class EditEmployeeComponent implements OnInit{
 
+  employeeDetails: Employee = {
+    id:'',
+    name:'',
+    email:'',
+    phone:0,
+    salary:0,
+    department:''
+  };
+
+  constructor(private route: ActivatedRoute, private employeeeService: EmployeesService,
+    private router: Router
+  ){ }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next:(params) => {
+        const id = params.get('id');
+
+        if (id) {
+            this.employeeeService.getEmployee(id).subscribe({
+              next: (response) => {
+                  this.employeeDetails = response;
+              }
+            });
+        }
+      }
+    })
+  }
+
+  updateEmployee(){
+    this.employeeeService.updateEmployee(this.employeeDetails.id, this.employeeDetails).subscribe({
+      next: (response) => {
+        this.router.navigate(['employees']);
+      }
+    });
+
+
+  }
 }
